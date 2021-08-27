0276a0c01415c731c011e0f99f5a4e15034f1ff8
FKinga92 <felfalusi.kinga@gmail.com>
2021-08-27 15:41:18 +0200

    use interfaces instead of classes

--- src/models/Menu.ts
+++ src/models/Menu.ts
@@ -1,14 +1,10 @@
-import { generateId } from "../utils/util";
-import MenuItem from "./MenuItem";
+import { generateId } from '../utils/util';
+import { MenuItem } from './MenuItem';
 
-export default class Menu {
-  public readonly id: number;
-  public readonly name: string;
-  public readonly items: MenuItem[];
-
-  constructor(name: string, items: MenuItem[]) {
-    this.name = name;
-    this.items = items ?? [];
-    this.id = generateId();
-  }
+export interface Menu {
+  id: number;
+  name: string;
+  items: MenuItem[];
 }
+
+export const getEmptyMenu = (): Menu => ({ id: generateId(), name: '', items: [] });
--- src/models/MenuItem.ts
+++ src/models/MenuItem.ts
@@ -1,13 +1,9 @@
-import { generateId } from "../utils/util";
+import { generateId } from '../utils/util';
 
-export default class MenuItem {
-  public readonly id: number;
-  public readonly name: string;
-  public readonly price: number;
-
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-    this.id = generateId();
-  }
+export interface MenuItem {
+  id: number;
+  name: string;
+  price: string;
 }
+
+export const getEmptyMenuItem = (): MenuItem => ({ id: generateId(), name: '', price: '0' });
